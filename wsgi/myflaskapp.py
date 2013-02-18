a97e0b0581063fccb4b4bd2eb95b343a6c80f979
uolter <wtraspad@yahoo.it>
2013-02-18 22:08:21 +0100

    url deep parameter and text in the index page

--- wsgi/myflaskapp.py
+++ wsgi/myflaskapp.py
@@ -4,6 +4,7 @@ import settings
 from bson import json_util
 import json
 from utils import json_encoders
+from datetime import datetime
 
 DEBUG = True
 
@@ -11,19 +12,32 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 
-@app.route("/list.json")
-def all():
+def _get_records(deep):
 
 	db = Connection(settings.mongodb_host, settings.mongodb_port)[settings.mongodb_database]
 
 	if settings.mongodb_user:
 		db.authenticate(settings.mongodb_user, settings.mongodb_password)
+
+	if deep == None: 
+		deep = 24
+
+	return db.records.find().limit(int(deep))
+
+
+@app.route("/list.json/<int:deep>")
+def all(deep=None):
+
+	
 	
 	'''
 	dataset = db.record.aggregate(
 		[
-			{"$match": {"date": {"$lte": "new Date()" }}},
+			{"$
+			match": {"date": {"$lte": "new Date()" }}},
 			{"$group":    
+
+
 				{ 	"_id": "$name",      
 					"trend": { "$push": { "speed": "$speed", "date": "$date"}}    
 				} 
@@ -32,24 +46,18 @@ def all():
 	)
 	'''
 
-	docs = db.records.find().limit(48)
-	
-	
+	docs = _get_records(deep)
+
 	return str(json.dumps(list(docs), cls=json_encoders.Encoder))
 
-@app.route("/")
-def index():
+@app.route("/<int:deep>")
+def index(deep=None):
 
-	db = Connection(settings.mongodb_host, settings.mongodb_port)[settings.mongodb_database]
-
-	if settings.mongodb_user:
-		db.authenticate(settings.mongodb_user, settings.mongodb_password)
-	
-	docs = db.records.find().limit(48)
+	docs = _get_records(deep)
 
 	#data = json.dumps(list(docs), cls=json_encoders.Encoder)
 	
-	return render_template('index.html', data=docs)
+	return render_template('index.html', data=docs, time=datetime.today())
 
 
 
