368c670d34c1e069ab00c03592eaa0437b82802b
uolter <wtraspad@yahoo.it>
2013-02-20 21:32:35 +0100

    compute the speed max values and disply them on the page

--- crawler/utils/mongodb.py
+++ crawler/utils/mongodb.py
@@ -2,17 +2,18 @@ from pymongo import Connection, ASCENDING, DESCENDING
 from datetime import datetime
 import settings
 import logging
+from bson.code import Code
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 logger.addHandler(settings.fh)
 
 
+db = Connection(settings.mongodb_host, settings.mongodb_port)[settings.mongodb_database]
+
 def save(records):
 
 	try:
-
-		db = Connection(settings.mongodb_host, settings.mongodb_port)[settings.mongodb_database]
 
 		if settings.mongodb_user:
 			db.authenticate(settings.mongodb_user, settings.mongodb_password)
@@ -24,9 +25,27 @@ def save(records):
 
 		db.records.insert(doc)
 
-	
+
 		db.records.create_index([("date", DESCENDING)])
+
+		# drop the max speed collection:
+		db.max_speed.remove()
+
+		# run nap reduce to compute the max speed for each canditate:
+		result = db.records.map_reduce(_map, _reduce, 'max_speed')
+
+		logger.info('Map Reduce ok')
+		logger.info(result)
+
+		db.max_speed.create_index([("value", ASCENDING)])
+
+
 
 	except Exception, e:
 
 		logger.error(e)
+
+_map = Code("function() { var max = 0; var name = ''; this.stat.forEach(function(val) { emit(val.name, val.speed); }); }")
+
+_reduce = Code("function(key, emits) { db.mr_log.insert({'message': 'reduce key ' + key + ' emits: ' + emits}); var max = emits[0]; emits.forEach(function(val){ if (val > max) max = val; }); return max; } ")
+
--- wsgi/myflaskapp.py
+++ wsgi/myflaskapp.py
@@ -10,13 +10,12 @@ DEBUG = True
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-
+_db = Connection(settings.mongodb_host, settings.mongodb_port)[settings.mongodb_database]
 def _get_records(deep):
 
-	db = Connection(settings.mongodb_host, settings.mongodb_port)[settings.mongodb_database]
-
+	
 	if settings.mongodb_user:
-		db.authenticate(settings.mongodb_user, settings.mongodb_password)
+		_db.authenticate(settings.mongodb_user, settings.mongodb_password)
 
 	if deep == None: 
 		deep = 24
@@ -24,30 +23,18 @@ def _get_records(deep):
 
 	d=datetime.today()-timedelta(hours = deep)
 
-	return db.records.find({"date": {"$gte":d }}).sort('date')
 
+
+	return _db.records.find({"date": {"$gte":d }}).sort('date')
+
+def _get_max_speeds():
+
+	return _db.max_speed.find().sort("value", -1)
+ 
 
 @app.route("/list.json/<int:deep>")
-def all(deep=None):
-
+def all(deep=None):	
 	
-	
-	'''
-	dataset = db.record.aggregate(
-		[
-			{"$
-			match": {"date": {"$lte": "new Date()" }}},
-			{"$group":    
-
-
-				{ 	"_id": "$name",      
-					"trend": { "$push": { "speed": "$speed", "date": "$date"}}    
-				} 
-			}
-		]
-	)
-	'''
-
 	docs = _get_records(deep)
 
 	return str(json.dumps(list(docs), cls=json_encoders.Encoder))
@@ -58,10 +45,15 @@ def index(deep=None):
 
 	docs = _get_records(deep)
 
+	max_speed = _get_max_speeds()
+
 	#data = json.dumps(list(docs), cls=json_encoders.Encoder)
 	
 
-	return render_template('index.html', data=docs, time=datetime.today())
+	return render_template('index.html', 
+		data=docs, 
+		time=datetime.today(),
+		max_speed = max_speed)
 
 
 
